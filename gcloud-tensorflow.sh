0435d1263c068b29a889ef16c68436ee3a8a7396
Akshay Dahiya <xadahiya@gmail.com>
2017-02-25 22:48:36 +0530

    Update gcloud-tensorflow.sh

--- gcloud-tensorflow.sh
+++ gcloud-tensorflow.sh
@@ -3,7 +3,7 @@ wget http://repo.continuum.io/archive/Anaconda3-4.0.0-Linux-x86_64.sh
 bash Anaconda3-4.0.0-Linux-x86_64.sh
 
 ## Activate anaconda path variables in the same shell session
-source ~/.bashrc
+# source ~/.bashrc
 
 ## Install tensorflow and tqdm modules
 conda install -c conda-forge tensorflow
